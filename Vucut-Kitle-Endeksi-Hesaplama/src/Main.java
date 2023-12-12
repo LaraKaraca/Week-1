24ce2109ce8362d43ff79c5c57e0dc68192abd45
Lara Karaca <152811987+LaraKaraca@users.noreply.github.com>
2023-12-13 02:19:02 +0300

    Vücut Kitle Endeksi Hesaplandı

--- Vucut-Kitle-Endeksi-Hesaplama/src/Main.java
+++ Vucut-Kitle-Endeksi-Hesaplama/src/Main.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double boy, kilo, indeks;
+
         Scanner inp = new Scanner(System.in);
         System.out.print("Lütfen Boyunuzu (metre cinsinden) giriniz : ");
-        double boy = inp.nextDouble();
+        boy = inp.nextDouble();
 
         System.out.print("Lütfen kilonuzu giriniz : ");
-        double kilo = inp.nextDouble();
+        kilo = inp.nextDouble();
 
-        double indeks = kilo / (boy + boy);
+        indeks = kilo / (boy * boy);
         System.out.println("Vücut Kitle İndeksiniz : " + indeks);
     }
 }
