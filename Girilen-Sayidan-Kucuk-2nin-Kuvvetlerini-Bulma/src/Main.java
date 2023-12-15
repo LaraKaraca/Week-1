3e58a0cef51303ea9770b775bd51b970172889b4
Lara Karaca <152811987+LaraKaraca@users.noreply.github.com>
2023-12-15 15:29:49 +0300

    Kuvetler Bulundu

---
+++ Girilen-Sayidan-Kucuk-2nin-Kuvvetlerini-Bulma/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+
+        System.out.println("Girdiğiniz sayıya kadar olan 4 ve 5'in kuvvetleri için;");
+        System.out.print("Sayı giriniz: ");
+        n=input.nextInt();
+
+        System.out.println("Girilen sayının 2'ün kuvvetleri");
+
+        for(int i=1; i<=n;i*=2)
+        {
+            System.out.println(i);
+        }
+
+        System.out.println("Girilen sayının 4'ün kuvvetleri");
+        for (int i=1;i<=n;i*=4)
+            System.out.println(i);
+
+        System.out.println("Girilen sayının 5'in kuvvetleri");
+        for (int i=1;i<=n;i*=5)
+            System.out.println(i);
+    }
+}
