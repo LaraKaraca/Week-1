88581916191d0720843c1b5c828e9de38397ee87
Lara Karaca <152811987+LaraKaraca@users.noreply.github.com>
2023-12-15 13:42:13 +0300

    Sınıf Geçme Durumu Hesaplandı

---
+++ Sinif-Gecme-Durumu/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double Matematik, Fizik, Turkce, Kimya, Muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Notunu Giriniz: ");
+        Matematik = input.nextDouble();
+
+        System.out.print("Fizik Notunu Giriniz: ");
+        Fizik = input.nextDouble();
+
+        System.out.print("Turkce Notunu Giriniz: ");
+        Turkce = input.nextDouble();
+
+        System.out.print("Kimya Notunu Giriniz: ");
+        Kimya = input.nextDouble();
+
+        System.out.print("Muzik Notunu Giriniz: ");
+        Muzik = input.nextDouble();
+
+        double totalNot = ((Matematik + Fizik + Turkce + Kimya + Muzik) / 5);
+
+        if (0 <= totalNot && totalNot <= 100) {
+            if (totalNot < 55) {
+                System.out.print("Uzgünüz Kaldınız");
+            } else {
+                System.out.print("Tebrikler Gectiniz");
+            }
+        } else {
+            System.out.print("Notunuz 0 ile 100 arasında değil ????");
+        }
+    }
+}
