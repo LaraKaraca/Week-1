b4774de4ad79a2b98d1d05a496cc339a493f2bc3
Lara Karaca <152811987+LaraKaraca@users.noreply.github.com>
2023-12-15 13:31:37 +0300

    Hesap Makinası Programı Oluşturuldu

--- Hesap-Makinasi-Programi/src/Main.java
+++ Hesap-Makinasi-Programi/src/Main.java
@@ -1,48 +1,44 @@
 import java.util.Scanner;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // degiskenler tanimlandi
+        int sayi1, sayi2, secim;
 
-        System.out.println("İşlem seçin: ");
-        System.out.println("1. Toplama: ");
-        System.out.println("2. Çıkarma: ");
-        System.out.println("3. Çarpma: ");
-        System.out.println("4. Bölme: ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("ilk sayiyi giriniz: ");
+        sayi1 = input.nextInt();
 
-        int secim = scanner.nextInt();
+        System.out.print("ikinci sayiyi giriniz: ");
+        sayi2 = input.nextInt();
 
-        System.out.println("Sayıları girin: ");
-        int sayi1 = scanner.nextInt();
-        int sayi2 = scanner.nextInt();
+        System.out.println("1-Toplama\n2-Çıkarna\n3-Carpma\n4-Bolme");
+        System.out.print("Seciminiz nedir?: ");
+        secim = input.nextInt();
+
+        // Switch case kullanarak islem secimini yapmak
 
         switch (secim) {
             case 1:
-                int toplam = sayi1 + sayi2;
-                System.out.println("Sonuç" + toplam);
+                secim = sayi1 + sayi2;
+                System.out.print("Toplama isleminizin sonucu: " + secim);
                 break;
-
             case 2:
-                int fark = sayi1 - sayi2;
-                System.out.println("Sonuç: " + fark);
+                secim = sayi1 - sayi2;
+                System.out.print("Cıkarma isleminizin sonucu: " + secim);
                 break;
-
             case 3:
-                int carpim = sayi1 * sayi2;
-                System.out.println("Sonuç: " + carpim);
-
+                secim = sayi1 * sayi2;
+                System.out.print("Carpma isleminizin sonucu: " + secim);
+                break;
             case 4:
-                if (sayi2!= 0){
-                double bolum = (double) sayi1 / sayi2;
-                System.out.println("Sonuç: " + bolum);
-            }else{
-                System.out.println("Bölme işlemi için ikinci sayı sıfır olamaz.");
-            }
-            break;
+                secim = sayi1 / sayi2;
+                System.out.print("Bolme isleminizin sonucu: " + secim);
+                break;
             default:
-                System.out.println("Geçersiz bir seçim yaptınız.");
-        }
-        scanner.close();
+                System.out.println("Gecersiz bir sayi girdiniz");
 
+        }
     }
 }
